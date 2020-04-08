fd7bc785aa6b6f5bb3408571d6155cfcc3b49028
Andrew Dinh <andrewd@benchling.com>
2020-04-07 22:22:49 -0700

    Add type annotation for coordinates

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -33,6 +33,6 @@ const logNumber: (i: number) => void = (i: number) => {
 // When to use annotations
 // 1).  Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json); //JSON.parse returns the any type.
+const coordinates: {x: number, y: number} = JSON.parse(json); //JSON.parse returns the any type.
 
 
